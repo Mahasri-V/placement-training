3242d445e09f380f0292967969cd9ad90e1107a5
Mahasri-V <151143352+Mahasri-V@users.noreply.github.com>
2024-08-01 21:32:49 +0530

    Random number.py

---
+++ 01.08.2024/Random number.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Generate random numbers from a uniform distribution
+rand_array = np.random.rand(3, 3)
+print("Random Array:\n", rand_array)
+
+# Generate random integers between 0 and 10
+rand_int_array = np.random.randint(0, 10, (2, 3))
+print("Random Integer Array:\n", rand_int_array
