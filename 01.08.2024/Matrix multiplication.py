05f66e1813835a99de37eef02c00950780a38b24
Mahasri-V <151143352+Mahasri-V@users.noreply.github.com>
2024-08-01 21:31:39 +0530

    Matrix multiplication.py

---
+++ 01.08.2024/Matrix multiplication.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Define 
+A = np.array([[1, 2], [3, 7]])
+B = np.array([[9, 7], [9, 8]])
+
+# Perform matrix multiplication
+C = np.dot(A, B)
+print("Matrix Multiplication Result:\n", C)
